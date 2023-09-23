c2bac63978e31ef91c7f322da7c25017ea62defe
julian1c2a <julian.calderon.almendros@gmail.com>
2023-09-23 19:33:17 +0200

    First commit.

---
+++ ArithmeticSeriesModule.py
@@ -0,0 +1,131 @@
+from numpy import longdouble as ldbl
+from numpy import ceil
+from numpy import floor
+from numpy import round
+from numpy import sqrt
+
+"""
+This module contains a function from the set of natural numbers 
+to the cartesian product of natural numbers with self.
+The naive function:
+    
+    SetN is the set of natural numbers included 0.
+    
+    SetN x SetN is the set of 2-tuples of natural numbers.
+    
+    linear_series is a bijective function.
+    
+    If group is a natural number defined as the sum of the projections of the resultant tuple:
+        group = 1st + 2nd, 
+    then group represents a group of tuples as images of consecutive indexes of natural numbers,
+    under the function linear_series.
+     
+    linear_series: SetN  --> SetN x SetN  # group
+                   0    |-> ( 0  ,  0 )  # 0
+                   1    |-> ( 0  ,  1 )  # 1
+                   2    |-> ( 1  ,  0 )  # 1
+                   3    |-> ( 0  ,  2 )  # 2
+                   4    |-> ( 1  ,  1 )  # 2
+                   5    |-> ( 2  ,  0 )  # 2
+                   6    |-> ( 0  ,  3 )  # 3
+                   7    |-> ( 1  ,  2 )  # 3
+                   8    |-> ( 2  ,  1 )  # 3
+                   9    |-> ( 3  ,  0 )  # 3
+                 ...    |-> ( ..., ...)  # ...
+               index    |-> (1st , 2nd)  # 1st+2nd
+               
+This module is used to help to determine a formula for linear_series and its 
+inverse function linear_series_inv.
+
+First, we determine the formula for the minimum (and the maximum) index 
+of a given group number.
+     min_index_from_group(group: int) -> int
+     max_index_from_group(group: int) -> int
+Their formulas are:
+     min_index_from_group(group) = (group * (group + 1)) // 2
+     max_index_from_group(group) = (group * (group + 3)) // 2
+"""
+
+
+def min_index_from_group(group: int) -> int:
+    """
+    Return the minimum index of the group.
+    The formula that relations the group
+    with the index is:
+    group * (group + 3) >= 2 * index >= group * (group + 1)
+    :param group: The group represents the sum of
+        the denominator, and the numerator of
+        the element, minus one.
+    :type group: Integer, int. A number for a group of consecutive images.
+    :return: The minimum index of the elements
+        in the series that is determined by the
+        group.
+    :rtype: Integer, int. An index.
+    """
+    return ((group + 1) * group) // 2
+
+
+def max_index_from_group(group: int) -> int:
+    """
+    Return the maximum index of the group.
+    The formula that relations the group
+    with the index is:
+    group * (group + 3) >= 2 * index >= group * (group + 1)
+    :param group: The group represents the sum of
+        the denominator, and the numerator of
+        the element, minus one.
+    :type group: Integer, int. A number for a group of consecutive images.
+    :return: The maximum index of the elements
+        in the series that is determined by the
+        group.
+    :rtype: Integer, int. An index.
+    """
+    return (group * (group + 3)) // 2
+
+
+def max_group_from_index(ind: int) -> int:
+    result: int = int(ceil(ldbl(sqrt(ldbl(8 * ind + 9)) - 3) / ldbl(2)))
+    return result
+
+
+def min_group_from_index(ind: int) -> int:
+    result: int = int(floor(ldbl(-1 + sqrt(ldbl(1 + 8 * ind))) / ldbl(2)))
+    return result
+
+
+def group_from_index(ind: int) -> int:
+    return int(round((min_group_from_index(ind) + max_group_from_index(ind)) / ldbl(2)))
+
+
+def index_in_group_interval(ind: int, group: int) -> bool:
+    return min_index_from_group(group) <= ind <= max_index_from_group(group)
+
+
+def linear_series(ind: int) -> tuple:
+    group: int = int(group_from_index(ind))
+    pr1st: int = ind - min_index_from_group(group)
+    pr2nd: int = group - pr1st
+    return pr1st, pr2nd
+
+
+def linear_series_inv(pr1st: int, pr2nd: int) -> int:
+    group: int = pr1st + pr2nd
+    index: int = pr1st + min_index_from_group(group)
+    return index
+
+
+def identity_in_SetN(index: int) -> int:
+    image_of_index: tuple[int, int] = linear_series(index)
+    return linear_series_inv(
+        image_of_index[0],
+        image_of_index[1]
+    )
+
+
+def identity_in_SetN_x_SetN(pr1st: int, pr2nd) -> tuple[int, int]:
+    index_image_of_tuple: int = linear_series_inv(pr1st, pr2nd)
+    return linear_series(index_image_of_tuple)
+
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,38 @@
+from ArithmeticSeriesModule import min_index_from_group as min_index
+from ArithmeticSeriesModule import max_index_from_group as max_index
+from ArithmeticSeriesModule import min_group_from_index as min_group
+from ArithmeticSeriesModule import max_group_from_index as max_group
+from ArithmeticSeriesModule import group_from_index
+from ArithmeticSeriesModule import linear_series as N2NxN_index
+from ArithmeticSeriesModule import linear_series_inv as NxN2N_tuple
+from ArithmeticSeriesModule import identity_in_SetN as id_SetN
+from ArithmeticSeriesModule import identity_in_SetN_x_SetN as id_SetN_x_SetN
+
+
+if __name__ == "__main__":
+    for group in range(0, 10):
+        print(
+            f"group {group} has indexes minimum {min_index(group)} and maximum {max_index(group)}"
+        )
+        for index in range(min_index(group), max_index(group) + 1):
+            print(
+                f"                                     index {index} has group {group_from_index(index)}"
+            )
+    for index in range(0, 55):
+        pr1st, pr2nd = N2NxN_index(index)
+        print(f"s({index}) = ({pr1st},{pr2nd})")
+        print(f"s_inv({pr1st},{pr2nd}) = ({NxN2N_tuple(pr1st,pr2nd)})")
+
+    correct: bool = True
+    for index in range(0, 10000):
+        correct = correct and (id_SetN(index) == index)
+    if correct:
+        print("Correct evaluation of the identity in SetN by composition of functions.")
+
+    correct = True
+    for pr1st in range(0, 1000):
+        for pr2nd in range(0, 1000):
+            correct = correct and (id_SetN_x_SetN(pr1st, pr2nd) == (pr1st, pr2nd))
+    if correct:
+        print("Correct evaluation of the identity in SetN x SetN by composition of functions.")
+
