b9b018820978820521119c29abc6938f94edf4a1
Julián Calderón Almendros <julian.calderon.almendros@gmail.com>
2025-03-12 18:24:38 +0000

    Refactor main.py for improved readability and consistency in output formatting

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
+"""
+This module performs various arithmetic series operations and evaluations.
+"""
+
+
 from ArithmeticSeriesModule import min_index_from_group as min_index
 from ArithmeticSeriesModule import max_index_from_group as max_index
-from ArithmeticSeriesModule import min_group_from_index as min_group
-from ArithmeticSeriesModule import max_group_from_index as max_group
 from ArithmeticSeriesModule import group_from_index
 from ArithmeticSeriesModule import quadratic_series as N2NxN_index
 from ArithmeticSeriesModule import quadratic_series_inv as NxN2N_tuple
 from ArithmeticSeriesModule import identity_in_SetN as id_SetN
 from ArithmeticSeriesModule import identity_in_SetN_x_SetN as id_SetN_x_SetN
 
-from numpy import longdouble as ldbl
-from numpy import ceil
-from numpy import floor
-from numpy import round
-from numpy import sqrt
-
 if __name__ == "__main__":
     for group in range(0, 10):
-        print(
-            f"group {group} has indexes minimum {min_index(group)} and maximum {max_index(group)}"
-        )
+        print(f"group {group} has indexes minimum {min_index(group)}"
+              f" and maximum {max_index(group)}")
         for index in range(min_index(group), max_index(group) + 1):
-            print(
-                f"                                     index {index} has group {group_from_index(index)}"
-            )
+            print(f"index {index} has group "
+                  f"{group_from_index(index)}")
     for index in range(0, 55):
         pr1st, pr2nd = N2NxN_index(index)
         print(f"s({index}) = ({pr1st},{pr2nd})")
         print(f"s_inv({pr1st},{pr2nd}) = ({NxN2N_tuple(pr1st,pr2nd)})")
 
-    correct: bool = True
+    CORRECT: bool = True
     for index in range(0, 10000):
-        correct = correct and (id_SetN(index) == index)
-    if correct:
-        print("Correct evaluation of the identity in SetN by composition of functions.")
+        CORRECT = CORRECT and (id_SetN(index) == index)
+    if CORRECT:
+        print("Correct evaluation of the identity in SetN by composition"
+              " of functions.")
     else:
-        print("Wrong evaluation of the identity in SetN by composition of functions.")
-
-    correct = True
+        print("Wrong evaluation of the identity in SetN by composition of"
+              " functions.")        
+    CORRECT = True
     for pr1st in range(0, 1000):
         for pr2nd in range(0, 1000):
-            correct = correct and (id_SetN_x_SetN(pr1st, pr2nd) == (pr1st, pr2nd))
-    if correct:
-        print("Correct evaluation of the identity in SetN x SetN by composition of functions.")
+            CORRECT = CORRECT and (id_SetN_x_SetN(pr1st, pr2nd) == (pr1st, pr2nd))
+    if CORRECT:
+        print("Correct evaluation of the identity in SetN x SetN by composition"
+              " of functions.")
     else:
-        print("Wrong evaluation of the identity in SetN x SetN by composition of functions.")
+        print("Wrong evaluation of the identity in SetN x SetN by composition"
+              " of functions.")
     # a : ldbl = 64
     # b : ldbl = 80
     # c : ldbl = 5
